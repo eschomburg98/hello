4e88e2d69269a6a0469961aa2155036d2b37fda1
eschomburg98 <emily.schomburg@husker.unl.edu>
2021-08-28 22:03:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Emily Schomburg
+ *         emily.schomburg@huskers.unl.edu
+ * Date: 2021/08/28
+ *
+ * Hack 1
+ *
+ */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Emily Schomburg!\n");
+
+  return 0;
+}
